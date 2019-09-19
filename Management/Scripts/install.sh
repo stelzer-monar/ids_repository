b666dbd688fbe760b95ec98598668b2ddf10d282
José Ernesto Stelzer Monar <stelzer-monar@Nymeria.local>
2019-09-18 22:53:03 -0300

    Use ppa to installation

--- Management/Scripts/install.sh
+++ Management/Scripts/install.sh
@@ -2,12 +2,19 @@
 set -e
 echo "Installing Suricata..."
 
-sudo apt-get -y install libpcre3 libpcre3-dbg libpcre3-dev build-essential autoconf automake libtool libpcap-dev libnet1-dev libyaml-0-2 libyaml-dev zlib1g zlib1g-dev libcap-ng-dev libcap-ng0 make libmagic-dev libjansson-dev libjansson4 pkg-config libnetfilter-queue-dev libnetfilter-queue1 libnfnetlink-dev libnfnetlink0
+sudo apt-get -y install libpcre3 libpcre3-dbg libpcre3-dev build-essential libpcap-dev libnet1-dev libyaml-0-2 libyaml-dev pkg-config zlib1g zlib1g-dev libcap-ng-dev libcap-ng0 make libmagic-dev libjansson-dev libnss3-dev libgeoip-dev liblua5.1-dev libhiredis-dev libevent-dev python-yaml rustc cargo libnetfilter-queue-dev libnetfilter-queue1 libnetfilter-log-dev libnetfilter-log1 libnfnetlink-dev libnfnetlink0
 
-VER=3.1
-wget "http://www.openinfosecfoundation.org/download/suricata-$VER.tar.gz"
-tar -xvzf "suricata-$VER.tar.gz"
-cd "suricata-$VER"
 
-./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var && make && make install-full
+
+#VER=4.1.2
+#wget "http://www.openinfosecfoundation.org/download/suricata-$VER.tar.gz"
+#tar -xvzf "suricata-$VER.tar.gz"
+#cd "suricata-$VER"
+
+#./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var && make && make install-full
+
+sudo add-apt-repository ppa:oisf/suricata-stable
+sudo apt update
+sudo apt install suricata jq
+
 
--- Management/Scripts/start.sh
+++ Management/Scripts/start.sh
@@ -2,3 +2,7 @@
 
 set -e
 
+#IFACE=`ip route get 8.8.8.8 | awk '{ print $5; exit}'`
+#sudo /usr/bin/suricata -c /etc/suricata/suricata.yaml -i $IFACE
+
+sudo systemctl start suricata
